24f0bb6c243d70810fc2e67b3867af58c890c84d
faqih-firdaus26 <firdausfaqih727@gmail.com>
2023-10-02 17:25:05 +0700

    update fitur

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    public function index(){
+        $data = array(
+            'title' => 'Home Page'
+        );
+
+        return view('home',$data);
+    }
+}
